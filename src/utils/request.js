1d22b264175426d80b1da88470bc8d0973f65172
Nicholas <32389766+NicholasCao@users.noreply.github.com>
2018-10-23 21:36:08 +0800

    update request

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import wepy from 'wepy'
 
-export default async function (options) {
+export default async function (option) {
   //文档->https://developers.weixin.qq.com/minigame/en/dev/api/network/request/wx.request.html?search-key=request
-  let response = await wepy.request(options)
+  let response = await wepy.request(option)
 }
